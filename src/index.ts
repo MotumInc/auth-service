eb1bf9788d8b8c739971c6b2d8625ff47ceb902f
Yaroslav <q.link0.p@gmail.com>
2020-03-14 22:34:27 +0200

    Added communication to the user-registry

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { config } from "dotenv-safe"
 import bodyparser from "body-parser";
-import { wrapAPI } from "./util/api"
 import { register, login, refresh, invalidate } from "./api"
 
 config()
@@ -10,5 +9,5 @@ const app = express();
 app.use(bodyparser.json())
 app.post("/register", register)
 app.post("/login", login)
-app.get("/refresh", wrapAPI(refresh))
-app.post("/invalidate", wrapAPI(invalidate))
+app.get("/refresh", refresh)
+app.post("/invalidate", invalidate)
--- src/util/user-registry.ts
+++ src/util/user-registry.ts
@@ -1,18 +1,72 @@
-import { UserQuery, User, AddUserRequest, UpdateUserQuery } from "../protobuf-gen/user-registry_pb"
+import { credentials } from "grpc"
+import {
+    UserQuery,
+    User,
+    AddUserRequest,
+    UpdateUserQuery,
+    VoidResponse
+} from "../protobuf-gen/user-registry_pb"
+import { UserRegistryClient } from "../protobuf-gen/user-registry_grpc_pb"
+import { response } from "express"
 
 export type Query = Partial<UserQuery.AsObject>
 
-//TODO
-export const getUser = async (query: Query): Promise<User.AsObject | null> => ({
-    name: "John",
-    id: "none",
-    login: "@mail",
-    hash: "",
-    tokenrevision: 0
-})
+const { USER_REGISTRY_URL } = process.env
 
-//TODO
-export const addUser = async (user: AddUserRequest.AsObject): Promise<User.AsObject | null> => ({ ...user, id: "none", tokenrevision: 0 })
+const client = new UserRegistryClient(USER_REGISTRY_URL!, credentials.createInsecure())
 
-//TODO
-export const updateUser = async (query: Query, modifications: Partial<Exclude<UpdateUserQuery.AsObject, "query">>): Promise<void> => {}
+const fetchUser = (query: UserQuery) =>
+    new Promise<User>((resolve, reject) => {
+        client.getUser(query, (err, res) => {
+            if (err) reject(err)
+            else resolve(res)
+        })
+    })
+
+const appendUser = (user: AddUserRequest) =>
+    new Promise<User>((resolve, reject) => {
+        client.addUser(user, (err, res) => {
+            if (err) reject(err)
+            else resolve(res)
+        })
+    })
+
+const manipulateUser = (query: UpdateUserQuery) =>
+    new Promise<VoidResponse>((resolve, reject) => {
+        client.updateUser(query, (err, res) => {
+            if (err) resolve(res)
+            else reject(err)
+        })
+    })
+
+export const getUser = async ({ id, login }: Query): Promise<User.AsObject | null> => {
+    const request = new UserQuery()
+    if (id !== undefined) request.setId(id)
+    if (login !== undefined) request.setLogin(login)
+    const response = await fetchUser(request)
+    return response.toObject()
+}
+
+export const addUser = async ({ name, login, hash }: AddUserRequest.AsObject): Promise<User.AsObject> => {
+    const request = new AddUserRequest()
+    request.setName(name)
+    request.setHash(hash)
+    request.setLogin(login)
+    const response = await appendUser(request)
+    return response.toObject()
+}
+
+export const updateUser = async (
+    { id, login: queryLogin }: Query,
+    { hash, login, name, tokenrevision }: Partial<Omit<UpdateUserQuery.AsObject, "query">>
+): Promise<void> => {
+    const request = new UpdateUserQuery()
+    const query = new UserQuery()
+    if (id !== undefined) query.setId(id)
+    if (queryLogin !== undefined) query.setLogin(queryLogin)
+    request.setQuery(query)
+    if (hash !== undefined) request.setHash(hash)
+    if (login !== undefined) request.setLogin(login)
+    if (name !== undefined) request.setName(name)
+    if (tokenrevision !== undefined) request.setTokenrevision(tokenrevision)
+}
