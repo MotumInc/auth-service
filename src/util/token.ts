b267f3b735caff638ce2bb41e78584924ebf8205
Yaroslav <q.link0.p@gmail.com>
2020-04-27 16:04:36 +0300

    added token expiration

--- src/util/token.ts
+++ src/util/token.ts
@@ -3,19 +3,20 @@ import { User } from "../protobuf-gen/user-registry_pb"
 
 export type Payload = Pick<User.AsObject, "id" | "login" | "tokenRevision">
 
-const generate = (user: Payload, key: string) => new Promise<string>((resolve, reject) => sign(user, key, (err, res) => {
-    if (err) reject(err)
-    else resolve(res)
-}))
+const generate = (user: Payload, key: string, expiresIn?: number | string) =>
+    new Promise<string>((resolve, reject) => sign(user, key, { expiresIn }, (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+    }))
 
 const verify = (token: string, key: string) => new Promise<object>((resolve, reject) => verifyBase(token, key, (err, res) => {
     if (err) reject(err)
     else resolve(res)
 }))
 
-export const generateAccessToken = (user: Payload) => generate(user, process.env.ACCESS_TOKEN_SECRET!)
+export const generateAccessToken = (user: Payload) => generate(user, process.env.ACCESS_TOKEN_SECRET!, "2m")
 
-export const generateRefreshToken = (user: Payload) => generate(user, process.env.REFRESH_TOKEN_SECRET!)
+export const generateRefreshToken = (user: Payload) => generate(user, process.env.REFRESH_TOKEN_SECRET!, "7d")
 
 export const verifyAccessToken = <T extends object>(token: string): Promise<T> => verify(token, process.env.ACCESS_TOKEN_SECRET!) as any
 
