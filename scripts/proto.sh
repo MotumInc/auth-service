a0b80c2d4692933b87793174d1edd4f78e621d4a
Yaroslav <q.link0.p@gmail.com>
2020-04-30 03:33:24 +0300

    Screw you gRPC for not letting me organise files into folders

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -7,23 +7,23 @@ npx grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${PROTO_DEST} \
 --grpc_out=${PROTO_DEST} \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-gRPC/user-registry.proto
+user-registry.proto
 
 # TS Typings
 npx grpc_tools_node_protoc \
 --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
 --ts_out=${PROTO_DEST} \
-gRPC/user-registry.proto
+user-registry.proto
 
 # JavaScript code generation
 npx grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${PROTO_DEST} \
 --grpc_out=${PROTO_DEST} \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-gRPC/auth.proto
+auth.proto
 
 # TS Typings
 npx grpc_tools_node_protoc \
 --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
 --ts_out=${PROTO_DEST} \
-gRPC/auth.proto
+auth.proto
